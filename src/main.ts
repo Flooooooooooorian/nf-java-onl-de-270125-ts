7a98fa8344815291430f1398844b48c502921f86
Flooooooooooorian <florian.weber@neuefische.de>
2025-03-04 10:12:39 +0100

    Add myFunction

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,3 @@
-
 console.log("Hello World");
 
 let name: string = "florian";
@@ -17,7 +16,7 @@ let isCoach: boolean = true;
 
 console.log(isCoach)
 
-let myObject: {name: string, age: number} = {age: 28, name: "Florian"}
+let myObject: { name: string, age: number } = {age: 28, name: "Florian"}
 
 console.log(myObject)
 console.log(myObject.name)
@@ -36,8 +35,7 @@ console.log("---------------")
 
 if (age === 10) {
     console.log("Age is lower that 10")
-}
-else {
+} else {
     console.log("Age is higher that 10")
 }
 
@@ -62,13 +60,48 @@ console.log(myValue2)
 
 if (1) {
     console.log("truthy")
-}
-else {
-    console.log("falsy")
+} else {
+    console.log(`falsy ${name} test`)
+    console.log(`falsy ` + name + ` test`)
 }
 
 console.log(5 / 0)
 
 let myValue3: any = 123
 
+console.log("---------------")
 
+function myFunction(myNumber: number, myString: string): string {
+    console.log(myString)
+    console.log(myNumber)
+    return myString + myNumber
+}
+
+const result: string = myFunction(13, "MyFunction")
+console.log(result)
+
+const myOtherFunction = (myNumber: number, myString: string): string => {
+    console.log(myString)
+    console.log(myNumber)
+    return myString + myNumber
+}
+
+console.log(myOtherFunction(15, "MyOtherFunction"))
+
+
+const names: string[] = ["Florian", "Dominic", "", "Martin"]
+
+const removedElement = names.pop()
+
+console.log(removedElement)
+
+if (removedElement !== undefined) {
+    console.log(names)
+}
+
+const newNames = names.filter(element => element)
+    .map(element => element.toLocaleLowerCase())
+    .map(element => element.charAt(0))
+    .some(element => element === 'f')
+
+console.log(newNames)
